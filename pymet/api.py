e192cc961f17e56fa1864114996fff988e9e43bf
andrew <andrewtheis4@gmail.com>
2019-05-06 07:37:06 -0700

    add typing module to main api

--- pymet/api.py
+++ pymet/api.py
@@ -1,31 +1,32 @@
 import collections
 import pandas as pd
+from typing import Dict, List
 
 from pymet.utils import constants
 
 
 class Met:
-    _path_to_csv = constants.CsvPaths.COLLECTION
+    _path_to_csv: str = constants.CsvPaths.COLLECTION
 
     def __init__(self):
         self.data = pd.read_csv(self._path_to_csv, encoding='utf8')
         self._clean_columns()
 
-    def _clean_columns(self):
+    def _clean_columns(self) -> None:
         self.data.columns = [
             col.lower().replace(' ', '_') for col in self.data.columns
         ]
 
 
 class MetPaintings(Met):
-    _path_to_csv = constants.CsvPaths.PAINTINGS
+    _path_to_csv: str = constants.CsvPaths.PAINTINGS
 
     def __init__(self):
         super().__init__()
-        self.data = self._process_collection_data()
+        self.data = self._drop_null_artists()
         self.lookup = self._create_artist_lookup()
 
-    def _process_collection_data(self):
+    def _drop_null_artists(self) -> pd.DataFrame:
         data = self.data.copy()
         data = data[constants.Columns.ARTIST_COLUMNS]
         return data.dropna(
@@ -33,17 +34,17 @@ class MetPaintings(Met):
             subset=[constants.Columns.ARTIST_ALPHA_SORT]
         ).reset_index(drop=True)
 
-    def _create_artist_lookup(self):
+    def _create_artist_lookup(self) -> Dict[str, List[str]]:
         lookup = collections.defaultdict(list)
         for artist in self.data.loc[:, constants.Columns.ARTIST_ALPHA_SORT]:
             last_name = artist.split(',')[0].lower()
             lookup[last_name].append(artist)
         return lookup
 
-    def contains_artist(self, artist):
+    def contains_artist(self, artist: str) -> bool:
         return artist.lower() in self.lookup.keys()
 
-    def works_by_artist(self, artist):
+    def works_by_artist(self, artist: str) -> pd.DataFrame:
         if not self.contains_artist(artist):
             raise RuntimeError(
                 f'Artist request {artist} not found in lookup of Met artists. '
