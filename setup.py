6c10af10ed12ffb954b28684f108314fd24eb699
andrew <andrewtheis4@gmail.com>
2019-05-07 08:26:08 -0700

    add dependencies to setup

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 from setuptools import setup
 
+dependencies = [
+    'attrdict',
+    'bs4',
+    'lxml',
+    'pandas',
+    'requests'
+]
+
 setup(
     name='pymet',
     version='0.0.1',
     packages=['pymet'],
     url='https://github.com/atheis4/met_api',
+    dependencies=dependencies,
     license='',
     author='Andrew',
     author_email='andrewtheis4@gmail.com',
